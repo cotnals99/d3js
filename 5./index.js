31b6a56d9aa7c8a83e41240fe1060488dd084a95
cotnals <su.chae@icloud.com>
2022-04-28 10:58:31 +0700

    BarChart with Labels

---
+++ 5./index.js
@@ -0,0 +1,77 @@
+//data
+
+let data = [40, 10, 20, 60, 30]
+let cities = [
+    { name: 'London', population: 8674000},
+    { name: 'New York', population: 8406000},
+    { name: 'Sydney', population: 4293000},
+    { name: 'Paris', population: 2244000},
+    { name: 'Beijing', population: 11510000}
+  ];
+  
+
+// Chart 1
+d3.select('.chart1')
+    .selectAll('circle')
+    .data(data)
+    .join('circle')
+    .attr('cx', (d, i)=>{return i*100+100})
+    .attr('cy', 50)
+    .attr('r', (d)=>{return 0.5*d})
+    .style('fill', (d)=>{return d>30 ? 'orange' : '#eee'})
+
+
+
+// Chart 2
+d3.select('.chart2')
+  .selectAll('circle')
+  .data(cities)
+  .join('circle')
+  .attr('cx', function(d, i) {
+    return i * 100+100;
+  })
+  .attr('cy', 150)
+  .attr('r', function(d) {
+    let scaleFactor = 0.000004;
+    let result = scaleFactor * d.population;
+    return result;
+  })
+  .style('fill', (d)=>{return d.population>5000000 ? 'orange' : '#aaa'});
+
+
+
+
+//Chart3
+d3.select('.bars')
+  .selectAll('rect')
+  .data(cities)
+  .join('rect')
+  .attr('width', (d)=>{
+      let scaleFactor = 0.00004;
+      console.log(d.population*scaleFactor);
+      return d.population * scaleFactor;
+  })
+  .attr('height', 19)
+  .attr('y', (d, i)=>{return i * 20})
+
+
+//Chart3 Label
+d3.select('.labels')
+  .selectAll('text')
+  .data(cities)
+  .join('text')
+  .attr('y', (d, i)=>{return i*20})
+  .text((d)=>{return d.name})
+
+
+//Chart3 Label
+d3.select('.labels2')
+  .selectAll('text')
+  .data(cities)
+  .join('text')
+  .attr('y', (d, i)=>{return i*20})
+  .attr('x', (d)=>{
+    let scaleFactor = 0.00004;
+    return d.population * scaleFactor + 5;
+  })
+  .text((d)=>{return d.population})
